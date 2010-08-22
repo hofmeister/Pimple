aae696abed773f9b3aa71e5ab0b0592b87180be4
hh <hh@772d010c-08bc-463a-a30a-e16bbb7839fa>
2010-08-22 18:41:27 +0000

    minor fixes
    git-svn-id: https://svn.newdawn.dk/svn/development/php/libs/beta/Pimple@3050 772d010c-08bc-463a-a30a-e16bbb7839fa

--- lib/taglib/CoreTagLib.php
+++ lib/taglib/CoreTagLib.php
@@ -1,15 +1,24 @@
 <?php
 class CoreTagLib {
+	private static $uidCount = 0;
+	public function uid() {
+		$uid = 'pimple-core-uid-'.(self::$uidCount);
+		self::$uidCount++;
+		return $uid;
+	}
+	public function tagUid() {
+		echo $this->uid();
+	}
     public function tagVar($attrs,$view) {
 		echo $view->data[$attrs->name];
     }
     public function tagTabPage($body,$attrs,$view) {
-        echo '<div class="tabpage">',chr(10),
+        echo '<div class="tabpage" id="'.$this->uid().'">',chr(10),
                 $body,chr(10),
             '</div>';
     }
     public function tagPage($body,$attrs,$view) {
-        echo '<div class="page">',chr(10),
+        echo '<div class="page" id="'.$this->uid().'">',chr(10),
 				$body,chr(10),
 			'</div>';
     }
