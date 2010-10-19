332c0e4fa2075bd447df8ff8a33e8c2a6b2c0f3f
Henrik <henrik@newdawn.dk>
2010-10-19 23:35:15 +0200

    added GUID()

--- lib/String.php
+++ lib/String.php
@@ -307,5 +307,16 @@ class String {
     public static function isAlphaNum($string) {
         return !preg_match('/[^A-Za-z0-9_]/','',$string);
     }
+    public static function GUID() {
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = substr($charid, 0, 8).$hyphen
+                .substr($charid, 8, 4).$hyphen
+                .substr($charid,12, 4).$hyphen
+                .substr($charid,16, 4).$hyphen
+                .substr($charid,20,12);
+        return $uuid;
+    }
 }
 
