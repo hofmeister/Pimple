52576eeaf8edc83dc4cadfb37c2904dee8bd36ad
Simon Sessingø <simon.sessingoe@gmail.com>
2011-03-07 15:35:42 +0100

    Sorting is now functional.

--- www/js/template/widget.js
+++ www/js/template/widget.js
@@ -17,6 +17,9 @@ $p.Widget.prototype = {
 	setData: function(data) {
 		this.data = data;
 		this.rows = this.data.rows;
+		/* Preset variables */
+		this.data.totalRows = (this.data.origTotalRows!=null) ? this.data.origTotalRows : this.data.rows.length;
+		this.data.rowsPerPage = this.data.totalRows;
 	},
 	setJSON: function(url) {
 		var c = this;
@@ -33,7 +36,7 @@ $p.Widget.prototype = {
 	render: function() {
 		this.container.html(this.template(this.data, this.guid));
 	},
-	setPaging: function(currentPage, rowsPerPage) {
+	setPaging: function(rowsPerPage) {
 		this.data.totalPages = Math.ceil(this.data.origTotalRows/rowsPerPage);
 		this.data.rowsPerPage = rowsPerPage;
 		this.data.totalRows = (this.data.origTotalRows > rowsPerPage) ? rowsPerPage : this.data.origTotalRows;
@@ -55,7 +58,25 @@ $p.Widget.prototype = {
 		this.data.currentPageIndex = parseInt(pageIndex);
 		this.data.rows = newRows;
 	},
-	setSort: function() {
-		// TODO: Make sorting...
+	setSort: function(field, order) {
+		this.data.sortOrder = order.toLowerCase();
+		this.rows.sort(function(x,y) {
+			var first = x[field];
+			if(!isNaN(parseInt(first))) {
+				if(order.toLowerCase() == 'desc') {
+					return x[field] - y[field];
+				}
+				return y[field] - x[field];
+			} else {
+				if(x[field]==y[field]){return 1;}
+	            if(order.toLowerCase() == 'asc'){
+	                if(x[field]<y[field]){return -1;}
+	            }else{
+	                if(x[field]>y[field]){return -1;}
+	            }
+	            return 1;
+			}
+		});
+		this.getPage(this.data.currentPageIndex);
 	}
 }
