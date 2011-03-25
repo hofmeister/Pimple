5214387bd203df134ec55a35445a209bea173f31
unknown <skipperbent@.(none)>
2011-03-25 15:00:23 +0100

    Added $p.string.addslashes(str) and $p.string.stripslashes(str) to the pimple.js, used in the javascript templates.

--- www/js/pimple.js
+++ www/js/pimple.js
@@ -270,6 +270,22 @@ var Pimple = {
             felm.blur();
         }
     },
+    string: {
+    	addslashes: function(str) {
+    		str=str.replace(/\\/g,'\\\\');
+    		str=str.replace(/\'/g,'\\\'');
+    		str=str.replace(/\"/g,'\\"');
+    		str=str.replace(/\0/g,'\\0');
+    		return str;
+    	},
+    	stripslashes: function(str) {
+    		str=str.replace(/\\'/g,'\'');
+    		str=str.replace(/\\"/g,'"');
+    		str=str.replace(/\\0/g,'\0');
+    		str=str.replace(/\\\\/g,'\\');
+    		return str;
+    	}
+    },
     format: {
 		date: function(format, str) {
 			return dateFormat(new Date(str), format);
